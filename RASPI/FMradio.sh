21df33091a2a5faa85c893b5b508c6211f9e7a1a
F3lda <kajel.jirgl@gmail.com>
2024-03-25 20:50:05 +0100

    RASPI AP WEB - FM radio over RTL start command

--- RASPI/FMradio.sh
+++ RASPI/FMradio.sh
@@ -10,6 +10,8 @@ echo "rtl_test"
 echo
 echo "rtl_fm -f 93.4e6 -s 200000 -r 48000 | aplay -r 48000 -f S16_LE"
 rtl_fm -f 93.4e6 -s 200000 -r 48000 | aplay -r 48000 -f S16_LE
+echo "rtl_fm -f 105.5e6 -s 200000 -r 48000 | ffmpeg -use_wallclock_as_timestamps 1 -f s16le -ac 1 -ar 48000 -i - -ac 2 -f pulse -device alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo 'stream-title'"
+echo
 echo
 echo "ps -e | grep rtl_fm"
 echo "kill <id>"
